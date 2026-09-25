88d543c234116b018b5f9f87a377d808714e03d3
Fabien S <ananajam@yahoo.com>
2026-09-25 15:19:30 +0200

    corrected function typo

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 // Launcher shim so `npx @11ty/eleventy` works when run from THIS directory too.
 // With the complete project (git, package.json, node_modules, netlify.toml, src/, config)
-module.exports = function(eleventyCcofig) {
+module.exports = function(eleventyConfig) {
   // Eleventy copie des dossiers vers le dossier de sortie (_site)
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
